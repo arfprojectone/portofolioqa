11e30386e0d9f765332747fe42e53597abd178ba
arfprojectone <arf.95kurniawan@gmail.com>
2022-01-21 23:53:20 +0700

    regression_test_21012022

--- Test Suites/Cura Healthcare Service/Regression Test.ts
+++ Test Suites/Cura Healthcare Service/Regression Test.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>Login and Make Appointment</name>
+   <name>Regression Test</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
