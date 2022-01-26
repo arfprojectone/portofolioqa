fa39869a50acd88641eb407a6993b6895c478c9f
arfprojectone <arf.95kurniawan@gmail.com>
2022-01-26 12:50:06 +0700

    bdd_cucumber_testing

---
+++ Test Suites/BDD Cucumber Demo/TestSuiteBDDCucumberDemo.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuiteBDDCucumberDemo</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>1809c51a-5057-4ab8-8c46-417bd9c4d9e7</testSuiteGuid>
+   <testCaseLink>
+      <guid>2904963d-ba1a-411c-aa46-272baccd973b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/BDD Cucumber Demo/RunFeatureFile</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
